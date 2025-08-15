75e3960756371b8afe368151c4d53f415215ff8f
An Ltd <contact@anweb.jp>
2025-08-15 16:26:18 +0900

    fix: fix types in toQuery

--- src/runtime/utils/toQuery.ts
+++ src/runtime/utils/toQuery.ts
@@ -1,9 +1,4 @@
-// TYPES
-type TValue = string | number | boolean | undefined | null
-export type TQueryObject = Record<string, TValue | TValue[]>
-
-
-export const toQuery = (params: TQueryObject): string => {
+export const toQuery = <T extends object>(params: T): string => {
 	return Object.entries(params)
 		.filter(([, value]) => value != null)
 		.map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`)
