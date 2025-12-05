e2394a75b22a90ed99be721ba8c4eab76d175f96
An Ltd <contact@anweb.jp>
2025-12-05 16:33:31 +0900

    fix: InfiniteScroll load in nextTick

--- src/runtime/composables/useAnList.ts
+++ src/runtime/composables/useAnList.ts
@@ -1,5 +1,5 @@
 import type { NitroFetchOptions, NitroFetchRequest } from 'nitropack'
-import { ref, reactive, watch, computed, type UnwrapRef } from 'vue'
+import { ref, reactive, watch, computed, type UnwrapRef, nextTick } from 'vue'
 import { useInfiniteScroll } from '@vueuse/core'
 import type { TInfiniteScroll, TResponseList } from '#ancore/types'
 import { useAnData } from './useAnData'
@@ -44,6 +44,8 @@ export const useAnList = <TData, TFilter extends object = {}>(initConfig: TConfi
 		}
 
 		setCount(data.data.value.count)
+
+		await nextTick()
 	}
 	const setCount = (value: number | null) => {
 		count.value = value
