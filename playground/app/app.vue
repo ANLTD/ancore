395bc3fb2e16765585057d6c44a4035f63f96b90
An Ltd <contact@anweb.jp>
2026-02-07 17:56:24 +0900

    feat: new component AnDropdown

--- playground/app/app.vue
+++ playground/app/app.vue
@@ -1,147 +1,27 @@
 <script setup lang="ts">
-	// import DialogTest from '~/components/DialogTest.vue'
 
-
-	// TRANSLATE
-	// const translate = {
-	// 	en: {
-	// 		GuideDashboard: 'Guide Dashboard',
-	// 		TouristDashboard: 'Tourist Dashboard'
-	// 	},
-	// 	ru: {
-	// 		GuideDashboard: 'Гид: Главная',
-	// 		TouristDashboard: 'Турист: Главная'
-	// 	}
-	// }
-
-
-	// TYPES
-	// interface TUser {
-	// 	id: string
-	// 	name: string
-	// 	info?: string
-	// }
-	// interface TForm {
-	// 	info: string
-	// 	text?: string
-	// }
-	// interface TFilter {
-	// 	info?: string
-	// 	limit?: number
-	// 	skip?: number
-	// }
-
-
-	// USE
-	// const { t, lang, set } = useAnI18n(translate)
-	// const Dialogs = useAnDialogs()
-
-
-	// DATA
-	// const show = ref(false)
-	// const form = useAnForm<TForm>({
-	// 	info: {
-	// 		default: 'sdf',
-	// 		rules: [
-	// 			{type: 'string', required: true, message: 'enter Info'}
-	// 		]
-	// 	},
-	// 	text: {
-	// 		default: ''
-	// 	}
-	// }, {info: undefined})
-	// const list = useAnList<TUser, TFilter>({
-	// 	request: '/api/users',
-	// 	filter: {info: form.state.value.info, limit: 5}
-	// })
-
-
-	// METHODS
-	// const setLang = (lng: string) => {
-	// 	if (set) {
-	// 		set(lng)
-	// 		location.reload()
-	// 	}
-	// }
-	// const openDialog = () =>{
-	// 	Dialogs.open(DialogTest)
-	// }
-
-
-	// WATCHES
-	// watch(() => form.state.value.info, async () => {
-	// 	const valid = await form.validator.check()
-	// 	if (!valid.pass) return
-	// 	list.filter.info = form.state.value.info
-	// 	list.filter.skip = 0
-	// })
-
-
-	// INIT
-	// list.filter.info = 'sdf'
-	// onMounted(() => {
-	// 	list.init()
-	// })
-	// await asyncInit(list.init)
-	// list.infiniteScroll({
-	// 	options: {distance: 100}
-	// })
 </script>
 
 <template>
   <div>
-    Nuxt module playground!<br><br>
-<!--	  <button @click="openDialog">Open dialog</button>-->
-<!--	  <br><br>-->
-<!--	  <div>-->
+	  <div v-for="i in 40" :key="i">{{ i }}</div>
+	  <AnDropdown class="button">
+		  <template #button="{toggle}">
+			  <div @click="toggle()">Button</div>
+		  </template>
 
-<!--	  <input v-model="form.state.value.info" />-->
-<!--		  <button @click="form.history.commit">Commit</button>-->
-<!--		  <button @click="form.history.reset">Reset</button>-->
-<!--		  <button @click="form.merge({info: 'tets'})">Merge</button>-->
-<!--	  </div>-->
-<!--	  <div>isChanged: {{form.history.isChanged}}</div>-->
-<!--	  <div>diff: {{form.diff.value}}</div>-->
-<!--	  <div>valid: {{  form.validator.errors.value.info }}</div>-->
+		  <template #menu="{ close }">
+			  <div @click="close">menu</div>
+		  </template>
+	  </AnDropdown>
 
-	  <Test />
-
-
-<!--	  <div @click="show= true"> {{list.count.value}}</div>-->
-<!--	  <div>-->
-<!--	  	<div v-for="item of list.items" :key="item.id">{{item}}</div>-->
-<!--	  </div>-->
-
-<!--	  <div>{{t('GuideDashboard')}}</div>-->
-<!--	  <div>-->
-<!--		  language: {{ lang }} <a @click="setLang('en')">EN</a><a @click="setLang('ru')">RU</a>-->
-<!--	  </div>-->
-
-<!--	  <AnTab :show="true">-->
-<!--		  123-->
-<!--	  </AnTab>-->
-<!--	  <AnTab :show="false">-->
-<!--		  333-->
-<!--	  </AnTab>-->
+	  <div v-for="i in 100" :key="'k' + i">{{ i }}</div>
   </div>
-	<AnDialogs />
 </template>
 
 <style>
-	:root {
-		--an-dialogs-background: rgba(0, 0, 0, 0.3);
+	.button {
+		display: inline-block;
+		width: fit-content;
 	}
-
-	.an-dialogs-enter-active, .an-dialogs-leave-active {
-		transition: opacity 0.3s ease;
-	}
-
-	.an-dialogs-enter-from, .an-dialogs-leave-to {
-		opacity: 0;
-	}
-
-	.an-dialogs-enter-to, .an-dialogs-leave-from {
-		opacity: 1;
-	}
-
 </style>
---
+++ src/runtime/components/An/Dropdown.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+	import { v4 } from 'uuid'
+	import { computed, onMounted, ref, useTemplateRef } from 'vue'
+	import { onClickOutside } from '@vueuse/core'
+
+
+	const props = defineProps<{
+		area?: string
+	}>()
+
+
+	// DATA
+	const id = ref<string>()
+	const state = ref<boolean>(false)
+	const refTarget = useTemplateRef('refTarget')
+
+
+	// METHODS
+	const toggle = (value?: boolean) => {
+		state.value = value !== undefined ? value : !state.value
+	}
+	const close = () => {
+		toggle(false)
+	}
+
+
+	// COMPUTED
+	const area = computed((): string => {
+		return props.area || 'bottom span-right'
+	})
+	const name = computed((): string => {
+		return id.value ? `--an-dropdown-${id.value}` : ''
+	})
+
+
+	// EVENTS
+	onMounted(() => {
+		id.value = v4()
+	})
+	onClickOutside(refTarget, close)
+</script>
+
+<template>
+	<div ref="refTarget" class="an-dropdown__button">
+		<slot name="button" :toggle="toggle" />
+
+		<teleport to="body">
+			<div v-show="state" class="an-dropdown__menu">
+				<slot name="menu" :close="close" />
+			</div>
+		</teleport>
+	</div>
+</template>
+
+<style scoped>
+	.an-dropdown__button {
+		anchor-name: v-bind(name);
+	}
+
+	.an-dropdown__menu {
+		position-anchor: v-bind(name);
+		position: fixed;
+		z-index: 1000;
+		position-area: v-bind(area);
+		position-try-fallbacks:
+			top span-right,
+			bottom span-left,
+			top span-left;
+	}
+</style>
