298baf4c5fbad4b69c0d5530bba49fd65244cc12
An Ltd <contact@anweb.jp>
2026-02-18 13:55:01 +0900

    refactor: remove dead try-catch in coreApi

--- src/runtime/utils/coreApi.ts
+++ src/runtime/utils/coreApi.ts
@@ -1,19 +1,11 @@
 import type { NitroFetchOptions, NitroFetchRequest } from 'nitropack'
-import type { FetchError } from 'ofetch'
 
 
 export const coreApi = <TData = unknown, TError = unknown>(
 	request: NitroFetchRequest,
 	opts: NitroFetchOptions<string>
 ): Promise<TData> => {
-	try {
-		return $fetch<TData>(
-			request,
-			{...opts}
-		)
-	} catch (error) {
-		throw error as FetchError<TError>
-	}
+	return $fetch<TData>(request, {...opts})
 }
 
 export type TApi = <TData, TError>(request: NitroFetchRequest, opts: NitroFetchOptions<string>) => Promise<TData>
